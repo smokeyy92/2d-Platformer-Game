6d0c189be5b0c05a4ca0a84e372fa62e36cf238b
Glenn Tanti <glenn.tanti@tipico.com>
2021-12-24 00:45:19 +0100

    finalizing lesson 4 shooting done

--- 2d platformer/Assets/Scripts/PlayerAttack.cs
+++ 2d platformer/Assets/Scripts/PlayerAttack.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+
     private Animator animate;
     private PlayerMovement playerMovement;
     private float cooldownTimer = Mathf.Infinity;
@@ -27,5 +30,18 @@ public class PlayerAttack : MonoBehaviour
     {
         animate.SetTrigger("attack");
         cooldownTimer = 0;
+
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+        //pool fireballs
+    }
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++) 
+        {
+            if(!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
     }
 }
--- 2d platformer/Assets/Scripts/Projectile.cs
+++ 2d platformer/Assets/Scripts/Projectile.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private float lifetime;
     private bool hit;
-    private float direction;
+    private float direction;    
 
-    private BoxCollider2D boxCollider;
     private Animator animate;
+    private BoxCollider2D boxCollider;
 
     private void Awake()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
         animate = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -20,6 +21,10 @@ public class Projectile : MonoBehaviour
         if (hit) return;
         float movementSpeed = speed * Time.deltaTime * direction;
         transform.Translate(movementSpeed, 0, 0);
+
+        lifetime += Time.deltaTime;
+
+        if (lifetime > 2) gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -30,6 +35,7 @@ public class Projectile : MonoBehaviour
     }
     public void SetDirection(float _direction)
     {
+        lifetime = 0;
         direction = _direction;
         gameObject.SetActive(true);
         hit = false;
