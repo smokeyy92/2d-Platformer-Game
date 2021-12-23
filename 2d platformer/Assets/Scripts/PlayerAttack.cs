9384b094378bbf934dd775be5c53a634a82be7dc
Glenn Tanti <glenn.tanti@tipico.com>
2021-12-23 16:39:47 +0100

    lesson 4 shooting

--- 2d platformer/Assets/Scripts/PlayerAttack.cs
+++ 2d platformer/Assets/Scripts/PlayerAttack.cs
@@ -17,7 +17,7 @@ public class PlayerAttack : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetMouseButton(0) && cooldownTimer > attackCooldown)
+        if(Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
             Attack();
 
         cooldownTimer += Time.deltaTime;
@@ -25,6 +25,7 @@ public class PlayerAttack : MonoBehaviour
 
     private void Attack()
     {
+        animate.SetTrigger("attack");
         cooldownTimer = 0;
     }
 }
